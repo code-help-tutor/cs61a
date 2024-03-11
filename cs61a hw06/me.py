2d781311680c458c5dae297fedffe60873963d38
Code Help Tutor <1823890830@qq.com>
2024-03-11 14:25:12 +0000

    WeChat cstutorcs cs61a

---
+++ cs61a hw06/me.py
@@ -0,0 +1,28 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+test = {
+  'name': 'Me',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> from data import *
+          >>> type(me.name) == str
+          True
+          >>> me.place is sather_gate
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
